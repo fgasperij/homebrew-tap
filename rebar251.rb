76e165e262c5467fd6292d031f9dcbac1c00085b
Fernando Gasperi Jabalera <fjabalera@manas.com.ar>
2015-11-06 12:20:47 -0300

    Rebar 2.5.1 (receiver).

---
+++ rebar251.rb
@@ -0,0 +1,17 @@
+class Rebar < Formula
+  desc "Erlang build tool"
+  homepage "https://github.com/rebar/rebar"
+  url "https://github.com/rebar/rebar/archive/2.5.1.tar.gz"
+  sha1 "cf8d3e33c21f09b826a52a681f5b729559a3280c"
+
+  depends_on "erlang"
+
+  def install
+    system "./bootstrap"
+    bin.install "rebar"
+  end
+
+  test do
+    system bin/"rebar", "--version"
+  end
+end
